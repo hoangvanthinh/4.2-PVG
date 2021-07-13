36efc99c488c2b1d97f2f8c93cc14f3d9ad163e0
Chien Tran <Chien Tran@DESKTOP-50LIQGL>
2021-07-13 15:10:37 +0700

    Change name header file

--- PV/Inverter/1_SunGrow.h
+++ PV/Inverter/1_SunGrow.h
@@ -28,8 +28,8 @@
 
 // This is a guard condition so that contents of this file are not included
 // more than once.  
-#ifndef XC_HEADER_TEMPLATE_H
-#define	XC_HEADER_TEMPLATE_H
+#ifndef XC_HEADER_SUNGROW_H
+#define	XC_HEADER_SUNGROW_H
 
 #include <xc.h> // include processor files - each processor file is guarded.  
 #include "0_Inverter.h"
