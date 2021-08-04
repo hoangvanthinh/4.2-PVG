c1de6eb6752dd399d264ebb2bcd902d2cfd91d69
TiDiChi <67595516+TiDiChi@users.noreply.github.com>
2021-08-04 18:17:10 +0700

    Updated 8/4/2021

--- PV/PV.c
+++ PV/PV.c
@@ -2,7 +2,7 @@
 
 CONTROL_INFOR gCtrlInfor;
 DEVICE_CTRL_INFOR gCtrlSetup;
-DEVICE_INFOR gCtrlResponse;
+DEVICE_CTRL_INFOR gCtrlResponse;
 
 void Device_RTU_Init(void)
 {
@@ -48,18 +48,30 @@ void Device_TCP_Init(void)
                 Weather_station_Init(&(G42.Dev_tcp[i].Dev_Setup));
                 break;
             default:
-                break;                
+                break;
         }
     }
 }
 
-void Device_RTU_GetData(uint8_t index)
+uint8_t Device_GetData(uint8_t index, char type)
 {
-    
-    switch(G42.Dev_rtu[index].Dev_Setup.Dev_type)
+    uint8_t device_type;
+    if(type == MODBUS_RTU)
+    {
+        device_type = G42.Dev_rtu[index].Dev_Setup.Dev_type;
+    }
+    else if(type == MODBUS_TCP)
+    {
+        device_type  = G42.Dev_tcp[index].Dev_Setup.Dev_type;
+    }
+    else
+    {
+        return 0;
+    }
+    switch(device_type)
     {
         case INVERTER:
-            Inverter_RTU_GetData(index);
+            Inverter_GetData(index,type);
             break;
         case PANEL:
             break;
@@ -68,18 +80,20 @@ void Device_RTU_GetData(uint8_t index)
         case CONTROL_IO:
             break;
         case WEATHER_STATION:
+            Weather_GetData(index);
             break;
         default:
             break;              
     }
+    return 1;
 }
 
-void Device_RTU_ResponseCtrl(void)
+void Device_ResponseCtrl(void)
 {
     switch(gCtrlInfor.Dev_Type)
     {
         case INVERTER:
-            Inverter_RTU_ResponseCtrl();
+            Inverter_ResponseCtrl(gCtrlInfor.Modbus_Type);
             break;
         case CONTROL_IO:
             break;
@@ -88,103 +102,22 @@ void Device_RTU_ResponseCtrl(void)
     }
 }
 
-void Device_TCP_GetData(uint8_t index)
+
+void Device_CtrlStrToBuffer(void)
 {
-    switch(G42.Dev_tcp[index].Dev_Setup.Dev_type)
+    switch(gCtrlInfor.Dev_Type)
     {
         case INVERTER:
-            Inverter_TCP_GetData(index);
-            break;
-        case PANEL:
-            break;
-        case STR_COMBINER:
+            Inverter_CtrlStrToBuffer(gCtrlInfor.Modbus_Type);
             break;
         case CONTROL_IO:
             break;
-        case WEATHER_STATION:
-            Weather_TCP_GetData(index);
-            break;
         default:
-            break;  
+            break;
     }
 }
 
 void Display_PV_paramenters(void)
 {
-    static uint8_t di=0;
-    switch(di)
-    {
-        case 0:
-        {
-            printf("\nType of device: INVERTER\n");
-            printf("Name device: SUN6KG03\n");
-            printf("Phase voltages: %d %d %d\n",InvData_RTU[0].AC.AN,InvData_RTU[0].AC.BN,InvData_RTU[0].AC.CN);
-            printf("Line voltages: %d %d %d\n",InvData_RTU[0].AC.VAB,InvData_RTU[0].AC.VBC,InvData_RTU[0].AC.VCA);
-            printf("Currents AC: %d %d %d\n",InvData_RTU[0].AC.IA,InvData_RTU[0].AC.IB,InvData_RTU[0].AC.IC);
-            printf("Power Factor: %d\n",InvData_RTU[0].AC.PF);
-            printf("Powers: %d %d %d\n",InvData_RTU[0].AC.P,InvData_RTU[0].AC.Q,InvData_RTU[0].AC.S);
-            printf("Frequency: %d\n",InvData_RTU[0].AC.F);
-            printf("Energy: %lld %lld %lld\n",InvData_RTU[0].AC.EP,InvData_RTU[0].AC.EQ,InvData_RTU[0].AC.ES);
-            printf("DC current: %d\n",InvData_RTU[0].DC.I_DC);
-            printf("DC voltage: %d\n",InvData_RTU[0].DC.U_DC);
-            printf("DC power: %d\n",InvData_RTU[0].DC.P_DC);
-            printf("Temperature cabinet: %d\n",InvData_RTU[0].T.T_Cabinet);
-            printf("Temperature heat sink: %d\n",InvData_RTU[0].T.T_Heat_Sink);
-            printf("Temperature transformer: %d\n",InvData_RTU[0].T.T_Tranformer);
-            printf("Operation state: %d\n",InvData_RTU[0].Operating_State);
-            printf("Event: %d\n",InvData_RTU[0].Event);
-            break;
-        }
-        case 1:
-        {
-            printf("\nType of device: INVERTER\n");
-            printf("Name device: SG110CX\n");
-            printf("Phase voltages: %d %d %d\n",InvData_TCP[0].AC.AN,InvData_TCP[0].AC.BN,InvData_TCP[0].AC.CN);
-            printf("Line voltages: %d %d %d\n",InvData_TCP[0].AC.VAB,InvData_TCP[0].AC.VBC,InvData_TCP[0].AC.VCA);
-            printf("Currents AC: %d %d %d\n",InvData_TCP[0].AC.IA,InvData_TCP[0].AC.IB,InvData_TCP[0].AC.IC);
-            printf("Power Factor: %d\n",InvData_TCP[0].AC.PF);
-            printf("Powers: %d %d %d\n",InvData_TCP[0].AC.P,InvData_TCP[0].AC.Q,InvData_TCP[0].AC.S);
-            printf("Frequency: %d\n",InvData_TCP[0].AC.F);
-            printf("Energy: %lld %lld %lld\n",InvData_TCP[0].AC.EP,InvData_TCP[0].AC.EQ,InvData_TCP[0].AC.ES);
-            printf("DC current: %d\n",InvData_TCP[0].DC.I_DC);
-            printf("DC voltage: %d\n",InvData_TCP[0].DC.U_DC);
-            printf("DC power: %d\n",InvData_TCP[0].DC.P_DC);
-            printf("Temperature cabinet: %d\n",InvData_TCP[0].T.T_Cabinet);
-            printf("Temperature heat sink: %d\n",InvData_TCP[0].T.T_Heat_Sink);
-            printf("Temperature transformer: %d\n",InvData_TCP[0].T.T_Tranformer);
-            printf("Operation state: %d\n",InvData_TCP[0].Operating_State);
-            printf("Event: %d\n",InvData_TCP[0].Event);
-            break;
-        }
-        case 2:
-        {
-            printf("\nType of device: Weather Station\n");
-            printf("Wind speed: %d\n",Weather[0].WIND_SPEED);
-            printf("Wind direction: %d\n",Weather[0].WIND_DIRECTION);
-            printf("Irradiation: %d\n",Weather[0].IRRADIATION);
-            printf("Temperature: %d\n",Weather[0].AMBIENT_T);
-            printf("Air humidity: %d\n",Weather[0].AIR_H);
-            printf("Atmospheric: %d\n",Weather[0].ATMOSPHERIC);
-            printf("Rain: %d\n",Weather[0].RAIN);
-            break;
-        }
-    }
-    di++;
-    if(di>2)
-        di=0;
 
-}
-
-void Device_RTU_CtrlStrToBuffer(void)
-{
-    switch(gCtrlInfor.Dev_Type)
-    {
-        case INVERTER:
-            Inverter_RTU_CtrlStrToBuffer();
-            break;
-        case CONTROL_IO:
-            break;
-        default:
-            break;
-    }
 }
