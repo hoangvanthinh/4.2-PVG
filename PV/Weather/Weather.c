d68a6915cadf670ea0f6ececb9286ea9baeb75ea
TiDiChi <67595516+TiDiChi@users.noreply.github.com>
2021-08-04 18:15:08 +0700

    Change function name to Weather_GetData

--- PV/Weather/Weather.c
+++ PV/Weather/Weather.c
@@ -1,6 +1,6 @@
 #include "../../SES.h"
 
-__eds__ __attribute ((eds))WEATHER_DATA Weather[MAX_WEATHER_TCP];
+__eds__ __attribute ((eds))WEATHER_DATA Weather[MAX_WEATHER];
 
 void Weather_station_Init(DEVICE_INFOR *Weather_Setup)
 {
@@ -10,9 +10,12 @@ void Weather_station_Init(DEVICE_INFOR *Weather_Setup)
     (*Weather_Setup).Fr[0].u16CoilsNo = 7;
     (*Weather_Setup).Fr[0].pointer = 0;
 }
-void Weather_TCP_GetData(uint8_t index)
+void Weather_GetData(uint8_t index)
 {
     static uint8_t I_Wea=0;
+    Weather[I_Wea].SID = G42.Dev_tcp[index].Dev_Setup.SID;
+    Weather[I_Wea].Series = G42.Dev_tcp[index].Dev_Setup.Series;
+    Weather[I_Wea].Modbus_Type =  MODBUS_TCP;
     Weather[I_Wea].WIND_SPEED = TCP_Buffer[index][0];
     Weather[I_Wea].WIND_DIRECTION = TCP_Buffer[index][1];
     Weather[I_Wea].IRRADIATION = TCP_Buffer[index][2];
@@ -20,15 +23,6 @@ void Weather_TCP_GetData(uint8_t index)
     Weather[I_Wea].AIR_H = TCP_Buffer[index][4];
     Weather[I_Wea].RAIN = TCP_Buffer[index][5];
     Weather[I_Wea].ATMOSPHERIC = TCP_Buffer[index][6];
-    printf("\nType of device: Weather Station\n");
-    printf("WIND_SPEED: %d\n", Weather[I_Wea].WIND_SPEED);
-    printf("WIND_DIRECTION: %d\n", Weather[I_Wea].WIND_DIRECTION);
-    printf("IRRADIATION: %d\n", Weather[I_Wea].IRRADIATION);
-    printf("AMBIENT_T: %d\n", Weather[I_Wea].AMBIENT_T);
-    printf("AIR_H: %d\n", Weather[I_Wea].AIR_H);
-    printf("RAIN: %d\n", Weather[I_Wea].RAIN);
-    printf("ATMOSPHERIC: %d\n", Weather[I_Wea].ATMOSPHERIC);
-
     I_Wea++;
     if(I_Wea >= G42.Num_weather)
     {
