8179464300e729fb689f628a14f78eaa7b6e947e
HoangVanThinh <thinh0702@gmail.com>
2021-08-24 11:11:23 +0700

    Merge Kmac
    Conflicts:
    	Multile_Meter/8_KMAC_MDM200.c
    	build/default/production/Framework/tcpip/src/common/big_int_helper.o.asm.d
    	dist/default/production/4.2-LI.X.production.elf
    	dist/default/production/4.2-LI.X.production.map
    	nbproject/Makefile-genesis.properties
    	nbproject/private/private.xml

--- nbproject/Makefile-variables.mk
+++ nbproject/Makefile-variables.mk
@@ -6,8 +6,8 @@
 CND_BASEDIR=`pwd`
 # default configuration
 CND_ARTIFACT_DIR_default=dist/default/production
-CND_ARTIFACT_NAME_default=4.2-PVG.production.hex
-CND_ARTIFACT_PATH_default=dist/default/production/4.2-PVG.production.hex
+CND_ARTIFACT_NAME_default=4.2-PVG.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/4.2-PVG.X.production.hex
 CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
-CND_PACKAGE_NAME_default=4.2-pvg.tar
-CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/4.2-pvg.tar
+CND_PACKAGE_NAME_default=4.2-pvg.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/4.2-pvg.x.tar
